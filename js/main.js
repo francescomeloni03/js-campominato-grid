77d5658ec5c7d91130b17d2796b1e72f08175326
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-26 16:33:26 +0200

    parte 2 (N.F)

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-
-
-
-
 const contentitore = document.getElementById("container");
+
+
 const btn = document.querySelector("button");
+
 
  btn.addEventListener("click", function ()  {
     for ( let contatore = 1; contatore < 101; contatore++){
         // console.log(contatore);
         contentitore.innerHTML += `<div class="box">${contatore}</div>`;
     }
-
  });
 
+ 
+
+ const box = document.querySelector("box");
+
+ box.addEventListener("click", function(){
+    contentitore.innerHTML += `<div class="box-azzurro">${contatore}</div>`;
 
 
-
-
+ });
 
 
 
